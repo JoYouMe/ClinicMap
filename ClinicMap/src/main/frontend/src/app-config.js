48138ab76e003962d8bef2f84ad740b77d830633
yuum <dyalsla0105@gmail.com>
2022-11-22 18:02:05 +0900

    서버 배포 완료

--- ClinicMap/src/main/frontend/src/app-config.js
+++ ClinicMap/src/main/frontend/src/app-config.js
@@ -2,6 +2,6 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname; //마지막 값이 변수에 담김
 
-backendHost = `http://${hostname}:8080`;
+backendHost = `https://${hostname}:8080`;
 
 export const API_BASE_URL = `${backendHost}`;
